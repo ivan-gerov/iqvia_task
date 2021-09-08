6710fa6bfcd9f936e18bb57e5aea430a6c3c8fed
Ivan Gerov <igerov11@gmail.com>
2021-09-08 19:51:16 +0300

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,20 @@
+
+import os
+from dotenv import dotenv_values
+
+class Config:
+    DEBUG = False
+    DEVELOPMENT = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "")
+
+class DevelopmentConfig(Config):
+    WORKSPACE_ROOT = os.path.sep.join(os.getenv("VIRTUAL_ENV").split("/")[:-1])
+    ENV_CONFIG_FILE = dotenv_values(f"{WORKSPACE_ROOT}/.development_env")
+    SQLALCHEMY_DATABASE_URI = ENV_CONFIG_FILE["DATABASE_URL"]
+    ENV = ENV_CONFIG_FILE["ENV"]
+
+
+class TestingConfig(Config):
+    TESTING = True
+
---
+++ app/models.py
@@ -0,0 +1,40 @@
+
+from main import db
+from marshmallow import fields, Schema, post_load
+
+class Contact(db.Model):
+    __tablename__ = "contacts"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+    
+    def __init__(self, username, first_name="", last_name=""):
+        print("Making a new contact")
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+            
+    
+    def __repr__(self):
+        return f"{self.username}:{getattr(self, 'first_name', '<first_name>')} {getattr(self, 'last_name', '<last_name>')}"
+
+db.create_all()
+
+class ContactSchema(Schema):
+    class Meta:
+        model = Contact
+        sqla_session = db.session
+    id = fields.Number(dump_only=True)
+    username = fields.String(required=True)
+    first_name = fields.String(required=True, data_key="first_name")
+    last_name = fields.String(required=True, data_key="last_name")
+    
+    @post_load
+    def make_contact(self, data, **kwargs):
+        return Contact(**data)
